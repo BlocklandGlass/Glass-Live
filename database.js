5f2b966be46bddc17aa8b061200c53f4537b82e3
Devon Endicott <devon.endicott@gmail.com>
2016-06-27 15:07:46 -0400

    works now

---
+++ database.js
@@ -0,0 +1,10 @@
+var checkAuthFromIdent = function checkAuthFromIdent(ident, ip) {
+  var request = require('sync-request');
+  var res = request('GET', "http://localhost/api/2/authCheck.php?ident=" + ident)
+
+  return JSON.parse(res.getBody());
+}
+
+module.exports = {
+  checkAuthFromIdent: checkAuthFromIdent
+}
--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 const net = require('net');
-
-const devMode = true;
+const database = require('./database.js');
 
 var authId = []; //authId[key] = blid
 var connectionByBlid = []; //connectionByBlid[blid] = connections[]
 
 const clientServer = net.createServer((c) => { //'connection' listener
-  console.log('client connected');
   c.on('end', () => {
-    console.log('client disconnected');
+    console.log('Client disconnected');
   });
 
   c.on('data', (data) => {
-    var field = data.toString().split('\t');
+    var field = data.toString().trim().split('\t');
 
     if(field[0] == 'auth') {
-      console.log('authing...');
-      if(authId[field[1]] != undefined) {
-        c.blid = authId[field[1]];
-      } else if(devMode) {
-        c.blid = 9789;
-      } else {
-        console.log('auth failed');
-        c.write('auth\tfailed\r\n');
-        c.delete();
+      console.log('Client connected (' + field[1] + ', ' + field[2] + ')');
+
+      var result = database.checkAuthFromIdent(field[1], c.remoteAddress);
+
+      if(result.status == "success") {
+        c.write('{"type":"auth", "status":"success"}\r\n');
+        c.blid = result.blid;
+      } else if(result.status = "failed") {
+        console.log('Failed');
+        c.write('{"type":"auth", "status":"failed"}\r\n');
+        //c.
         return;
       }
 
-      console.log('authed as ' + c.blid);
-      c.write('auth\tsuccess\r\n');
       if(connectionByBlid[c.blid] == undefined)
         connectionByBlid[c.blid] = [];
 
       connectionByBlid[c.blid].push(c);
+
+      pushNotification(c, "Glass Beta", "Welcome to the beta! Thanks for testing :)", "star", "5000", "");
+      //pushNotification(c, "Blockoworld", "Blockoworld is happening RIGHT NOW! Click me for more information.", "bricks", "0", "");
     }
+  });
+
+  c.on('error', (err) => {
+    console.error('Caught error', err);
   });
 
   c.write('hello\r\n');
@@ -46,21 +51,19 @@ const noteServer = net.createServer((c) => { //'connection' listener
   });
 
   c.on('data', (data) => {
-    //notification \t blid \t header \t text \t image \t action \t sticky
-    var field = data.toString().split('\t');
+    obj = JSON.parse(data);
 
-    if(field[0] == 'notification') {
-      console.log('receiving notification');
+    if(obj.type == 'notification') {
 
-      if(connectionByBlid[field[1]] == undefined) {
+      if(connectionByBlid[obj.target] == undefined) {
         console.log('target not found');
         return;
       }
 
-      for(var i = 0; i < connectionByBlid[field[1]].length; i++) {
-        con = connectionByBlid[field[1]][i];
+      for(var i = 0; i < connectionByBlid[obj.target].length; i++) {
+        con = connectionByBlid[obj.target][i];
         try {
-          con.write(data.toString() + '\r\n');
+          pushNotification(con, obj.title, obj.text, obj.image, obj.duration, obj.callback);
         } catch(err) {
           console.error('Caught error sending to client', err);
         }
@@ -75,9 +78,24 @@ const noteServer = net.createServer((c) => { //'connection' listener
 });
 
 clientServer.listen(27000, () => { //'listening' listener
-  console.log('client server bound');
+  console.log('Bound 27000');
 });
 
 noteServer.listen(27001, () => { //'listening' listener
-  console.log('note server bound');
+  console.log('Bound 27001\r\n');
 });
+
+function pushNotification(con, title, text, image, duration, callback) {
+  dat = {
+    "type":"notification",
+    "title":title,
+    "text":text,
+    "image":image,
+    "duration":duration,
+    "callback":callback
+  };
+
+  str = JSON.stringify(dat);
+  //console.log(str);
+  con.write(str + '\r\n');
+}
