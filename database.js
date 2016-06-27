3bffb8bc9e43dc4f5dcde8cef6dc4a3b4cb8cb4a
Devon Endicott <devon.endicott@gmail.com>
2016-06-27 15:14:33 -0400

    missing dep

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 var checkAuthFromIdent = function checkAuthFromIdent(ident, ip) {
   var request = require('sync-request');
-  var res = request('GET', "http://localhost/api/2/authCheck.php?ident=" + ident)
+  var res = request('GET', "http://test.blocklandglass.com/api/2/authCheck.php?ident=" + ident)
 
   return JSON.parse(res.getBody());
 }
