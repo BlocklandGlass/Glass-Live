423d32b3f106b97087e637ec2bbb82843e234e2e
Devon Endicott <devon.endicott@gmail.com>
2016-11-05 17:27:30 -0400

    glassbot

--- glassBot.js
+++ glassBot.js
@@ -1,6 +1,39 @@
 const moment = require('moment');
 
+module.greetings = [
+  "That's me!",
+  "How's it going?",
+  "Hello!",
+  "Yes?"
+];
+
+module._racialSlurs = [
+  "nigger",
+  "nig",
+  "nigg",
+  "beaner",
+  "faggot",
+  "fag",
+  "sand-nigger",
+  "sandnigger",
+  "spic"
+]
+
 var onRoomMessage = function(room, sender, message) {
+  if(sender.roomMessageHistory == null)
+    sender.roomMessageHistory = [];
+
+  checkMessageHistory(sender, message, room);
+
+  sender.roomMessageHistory.push({
+    time: moment().unix(),
+    message: message,
+    room: room.id
+  });
+
+  if(sender.roomMessageHistory.length > 100)
+    sender.roomMessageHistory.splice(0, sender.roomMessageHistory.length-100);
+
   if(message == message.toUpperCase() && message.length > 5) {
     sendRoomMessage(room, "No yelling, please!");
     issueWarning(sender, 1, room);
@@ -11,13 +44,13 @@ var onRoomMessage = function(room, sender, message) {
     var word = words[i];
     if(word == "@glassbot") {
       if(module._lastHello == null || moment().diff(moment.unix(module._lastHello), 'seconds') > 45) {
-        sendRoomMessage(room, "That's me!");
+        sendRoomMessage(room, module.greetings[Math.floor(Math.random() * module.greetings)]);
         module._lastHello = moment().unix();
       }
     }
 
     if(word == "nigger" || word == "nig" || word == "nigg") {
-      sendRoomMessage(room, "Racial slurs are not welcome here.");
+      sendRoomMessage(room, "Discimination is not welcome here.");
 
       setTimeout(()=>{
         room.sendObject({
@@ -25,7 +58,7 @@ var onRoomMessage = function(room, sender, message) {
           id: room.id,
           text: '<color:9b59b6> * GlassBot banned ' + sender.username + ' for ' +  60*5 + ' seconds'
         });
-        doRoomsBan(sender, 60*5, "Racial slurs are not welcome")
+        doRoomsBan(sender, 60*5, "Discimination is not welcome")
       }, 1100);
     }
   }
@@ -47,6 +80,35 @@ var sendRoomMessage = function(room, message) {
     timestamp: moment().unix(),
     datetime: moment().format('h:mm:ss a')
   });
+}
+
+var checkMessageHistory = function(sender, message, room) {
+  var hist = sender.roomMessageHistory.slice(0).reverse();
+
+  if(hist[4] != null) {
+    if(moment().diff(moment.unix(hist[4].time), 'seconds') < 5) {
+      sendRoomMessage(room, "Slow down there, sparky!");
+      issueWarning(sender, 1, room);
+    }
+  }
+
+  var repCount = 0;
+  for(var i = 0; i < Math.min(5, hist.length); i++) {
+    var msgObj = hist[i];
+
+    if(moment().diff(moment.unix(msgObj.time), 'seconds') > 60)
+      break; //anything older than a minute can't be too spammy
+
+    if(msgObj.message == message) {
+      repCount++;
+    }
+
+    if(repCount >= 2) {
+      sendRoomMessage(room, "Don't repeat yourself so often");
+      issueWarning(sender, 1, room);
+      break;
+    }
+  }
 }
 
 var doRoomsBan = function(client, duration, reason) {
