f7b1c316ecc7630aa491aa066b6ff16bd8f33067
Devon Endicott <devon.endicott@gmail.com>
2016-11-06 14:32:07 -0500

    more color tweaks

--- roomCommands.js
+++ roomCommands.js
@@ -35,7 +35,7 @@ var newCommandSet = function(room) {
       command['glassUpdate'] = "<version>\tNotifies clients a update is available";
     }
 
-    var msg = "<color:ed7669>Help:<br><color:444444>Public Commands:";
+    var msg = "<color:444444>Help:<br>Public Commands:";
     var i = 0;
     for(cmd in command) {
       if(msg != "")
@@ -46,12 +46,12 @@ var newCommandSet = function(room) {
       }
       i++;
 
-      msg = msg + "<color:ed7669>/" + cmd;
+      msg = msg + "<color:e74c3c>/" + cmd;
       if(command[cmd].indexOf('\t') > -1) {
         var field = command[cmd].split('\t');
-        msg = msg + " " + field[0] + " <color:444444>" + field[1];
+        msg = msg + " " + field[0] + " <color:555555>" + field[1];
       } else {
-        msg = msg + " <color:444444>" + command[cmd];
+        msg = msg + " <color:555555>" + command[cmd];
       }
     }
 
