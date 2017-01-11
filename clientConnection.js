fe598bc67aa8fc49f3fde2fc6c4b0d7017a4f018
Jincux <devon.endicott@gmail.com>
2017-01-11 17:09:09 -0500

    obj

--- clientConnection.js
+++ clientConnection.js
@@ -187,7 +187,7 @@ var createNew = function(socket) {
           logger.log('...running depreciated version ' + data.version);
         }
 
-        if(verParts[0] > 3 || version.indexOf("indev") > -1 || version.indexOf("beta") > -1) {
+        if(verParts[0] > 3 || data.version.indexOf("indev") > -1 || data.version.indexOf("beta") > -1) {
           logger.log('...running Glass in-dev ' + data.version);
           connection.isBeta = true;
         }
